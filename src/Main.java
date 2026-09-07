47172321c276e3c4fe61b5cb73d5d83f7bb2704a
J Subraya Shenoy <jsubrayashenoy@gmail.com>
2026-09-07 12:14:32 +0530

    Add main class to launch SimulatorUI

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.SwingUtilities;
+import src.ui.SimulatorUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            new SimulatorUI();
+        });
+    }
+}
